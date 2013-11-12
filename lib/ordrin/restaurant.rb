478f858901ca9629db7f3c76af61e197e1da3d55
Austen Ito <austen.ito@gmail.com>
2013-11-11 22:16:50 -0500

    Remove delivery class methods.

--- lib/ordrin/restaurant.rb
+++ lib/ordrin/restaurant.rb
@@ -1,27 +1,26 @@
 module OrdrIn
   class Restaurant < Model
-    attr_accessor :check, :delivery_fee, :restaurant_details
 
     def delivery_check(params)
-      self.check ||= OrdrIn::Delivery.check(id, params)
+      url = URI.escape("dc/#{id}/#{params[:date_time]}/#{params[:zip_code]}/#{params[:city]}/#{params[:address]}")
+      response = OrdrIn::RestaurantRequest.get(url)
+      OrdrIn::DeliveryCheck.new(response.body)
     end
 
-    def fee(params)
-      self.delivery_fee ||= OrdrIn::Delivery.fee(id, params)
+    def delivery_fee(params)
+      url = URI.escape("fee/#{id}/#{params[:subtotal]}/#{params[:tip]}/#{params[:date_time]}/#{params[:zip_code]}/#{params[:city]}/#{params[:address]}")
+      response = OrdrIn::RestaurantRequest.get(url)
+      OrdrIn::DeliveryFee.new(response.body)
     end
 
     def details
-      self.restaurant_details ||= OrdrIn::Restaurant.details(id)
+      OrdrIn::RestaurantDetails.new(OrdrIn::RestaurantRequest.get("rd/#{id}").body)
     end
 
     def self.deliveries(params)
       url = URI.escape("dl/#{params[:date_time]}/#{params[:zip_code]}/#{params[:city]}/#{params[:address]}")
       response = OrdrIn::RestaurantRequest.get(url)
       response.body.collect { |attributes| OrdrIn::Restaurant.new(attributes) }
-    end
-
-    def self.details(restaurant_id)
-      OrdrIn::RestaurantDetails.new(OrdrIn::RestaurantRequest.get("rd/#{restaurant_id}").body)
     end
   end
 end
--- spec/requests/restaurant_spec.rb
+++ spec/requests/restaurant_spec.rb
@@ -14,14 +14,6 @@ describe OrdrIn::Restaurant do
     end
   end
 
-  context ".details", :vcr do
-    it "returns restaurant details" do
-      details = OrdrIn::Restaurant.details(147)
-      details.restaurant_id.should == "147"
-      details.city.should == "College Station"
-    end
-  end
-
   context "#details", :vcr do
     it "returns restaurant details" do
       details = OrdrIn::Restaurant.new(id: 147).details
@@ -33,22 +25,22 @@ describe OrdrIn::Restaurant do
   context "#delivery_check" do
     it "returns delivery check", :vcr do
       restaurant = OrdrIn::Restaurant.new(id: 147)
-      delivery_check = restaurant.delivery_check(date_time: "ASAP", address: "1 Main Street", 
+      delivery_check = restaurant.delivery_check(date_time: "ASAP", address: "1 Main Street",
                                                   zip_code: 77840, city: "College Station")
       delivery_check.rid = 147
       delivery_check.meals = [0, 4]
     end
   end
 
-  context "#fee" do
-    it "returns fee", :vcr, record: :all do
+  context "#delivery_fee" do
+    it "returns delivery_fee", :vcr, record: :all do
       restaurant = OrdrIn::Restaurant.new(id: 147)
 
-      fee = restaurant.fee(subtotal: 20.42, tip: 5.05, date_time: "ASAP", 
-                           address: "1 Main Street", zip_code: 77840, 
-                           city: "College Station")
-      fee.rid = 147
-      fee.tax = 2.01
+      delivery_fee = restaurant.delivery_fee(subtotal: 20.42, tip: 5.05, date_time: "ASAP",
+                                             address: "1 Main Street", zip_code: 77840, 
+                                             city: "College Station")
+      delivery_fee.rid = 147
+      delivery_fee.tax = 2.01
     end
   end
 end
